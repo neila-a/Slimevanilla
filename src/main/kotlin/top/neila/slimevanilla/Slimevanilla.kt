c6d0d6bb8df49bd1ecb8334935686c1f60854f3f
Neil Alen <neilaspace@outlook.com>
2026-08-24 14:06:34 +0800

    🐛 修正合成材料和输出的数量

--- src/main/kotlin/top/neila/slimevanilla/Slimevanilla.kt
+++ src/main/kotlin/top/neila/slimevanilla/Slimevanilla.kt
@@ -2,10 +2,11 @@ package top.neila.slimevanilla
 
 const val rowWidth = 3
 const val startChar = 'A'
+const val originalBugTrackerURL = "https://github.com/neila-a/Slimevanilla/issues"
 
 class Slimevanilla : SlimevanillaBase() {
     override fun getJavaPlugin() = this
-    override fun getBugTrackerURL() = "https://github.com/neila-a/Slimevanilla/issues"
+    override fun getBugTrackerURL() = originalBugTrackerURL
 
     override fun onEnable() {
         server.pluginManager.registerEvents(SlimevanillaListener(), this)
--- src/main/kotlin/top/neila/slimevanilla/SlimevanillaListener.kt
+++ src/main/kotlin/top/neila/slimevanilla/SlimevanillaListener.kt
@@ -1,20 +1,37 @@
 package top.neila.slimevanilla
 
+import io.github.thebusybiscuit.slimefun4.api.recipes.RecipeType
 import io.github.thebusybiscuit.slimefun4.implementation.Slimefun
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.inventory.PrepareItemCraftEvent
+import org.bukkit.inventory.ItemStack
+
+val emptyMatrix = arrayOfNulls<ItemStack>(9)
+
+val toFlatItemStacks = { itemStacks: Array<ItemStack?> ->
+    itemStacks.map { stack ->
+        if (stack == null) return@map null
+        val flatStack = stack.clone()
+        flatStack.amount = 1
+        return@map flatStack
+    }
+}
 
 class SlimevanillaListener : Listener {
     @EventHandler
     fun onPrepareItemCraft(event: PrepareItemCraftEvent) {
         val inventory = event.inventory
         val matrix = inventory.matrix
-        Slimefun.getRegistry().enabledSlimefunItems.forEach { item ->
-            if (item != null) {
-                if (item.recipe.contentEquals(matrix)) {
-                    inventory.result = item.item
-                }
+
+        if (matrix.contentEquals(emptyMatrix)) return
+
+        val flatMatrix = toFlatItemStacks(matrix)
+        Slimefun.getRegistry().enabledSlimefunItems.filterNotNull().forEach { item ->
+            if (item.recipeType != RecipeType.ENHANCED_CRAFTING_TABLE) return@forEach
+
+            if (toFlatItemStacks(item.recipe) == flatMatrix) {
+                inventory.result = item.recipeOutput
             }
         }
     }
