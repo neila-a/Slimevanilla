d0943a78bd26cbaf32c441899cecd5a8aec3ebc3
Neil Alen <neilaspace@outlook.com>
2026-08-25 00:09:20 +0800

    💩 区分增强型工作台和原版工作台

--- src/main/kotlin/top/neila/slimevanilla/SlimevanillaListener.kt
+++ src/main/kotlin/top/neila/slimevanilla/SlimevanillaListener.kt
@@ -1,6 +1,6 @@
 package top.neila.slimevanilla
 
-import io.github.thebusybiscuit.slimefun4.api.events.ResearchUnlockEvent
+import io.github.thebusybiscuit.slimefun4.api.events.MultiBlockInteractEvent
 import io.github.thebusybiscuit.slimefun4.api.recipes.RecipeType
 import io.github.thebusybiscuit.slimefun4.implementation.Slimefun
 import org.bukkit.event.EventHandler
@@ -9,9 +9,16 @@ import org.bukkit.event.Event.Result
 import org.bukkit.event.inventory.PrepareItemCraftEvent
 import org.bukkit.event.EventPriority
 import org.bukkit.inventory.ItemStack
+import org.bukkit.inventory.MenuType.CRAFTING
 import org.bukkit.event.inventory.CraftItemEvent
 import io.github.thebusybiscuit.slimefun4.api.player.PlayerProfile
+import io.github.thebusybiscuit.slimefun4.implementation.items.multiblocks.EnhancedCraftingTable
 import org.bukkit.NamespacedKey
+import org.bukkit.entity.HumanEntity
+import org.bukkit.entity.Player
+import org.bukkit.event.inventory.InventoryCloseEvent
+import java.util.UUID
+import java.util.function.Consumer
 
 val emptyMatrix = arrayOfNulls<ItemStack>(9)
 
@@ -23,25 +30,78 @@ val Array<ItemStack?>.flat
         return@map flatStack
     }
 
+fun HumanEntity.toSlimefun(callback: Consumer<PlayerProfile>) = PlayerProfile.fromUUID(uniqueId, callback)
+
 class SlimevanillaListener(instance: Slimevanilla) : Listener {
     private var slimevanillaInstance: Slimevanilla? = null
+
     init {
         slimevanillaInstance = instance
         instance.server.pluginManager.registerEvents(this, instance)
     }
+
+    private val playerVanillaDiscoverdRecipes = mutableMapOf<UUID, Collection<NamespacedKey>>()
+    private val playerOpeningEC = mutableMapOf<UUID, Boolean>()
+
+    @EventHandler(priority = EventPriority.LOW)
+    fun onMultiBlockInteract(event: MultiBlockInteractEvent) {
+        val multiBlock = event.multiBlock
+        val player = event.player
+        player.toSlimefun { profile ->
+            val item = multiBlock.slimefunItem
+            if (!profile.hasUnlocked(item.research)) return@toSlimefun
+
+            if (item is EnhancedCraftingTable) {
+                event.isCancelled = true
+                
+                playerVanillaDiscoverdRecipes[player.uniqueId] = player.discoveredRecipes
+                player.undiscoverRecipes(player.discoveredRecipes)
+                player.discoverRecipes(profile.researches.map { it.affectedItems.map { item ->
+                    if (item.recipeType == RecipeType.ENHANCED_CRAFTING_TABLE) {
+                        val key = NamespacedKey(slimevanillaInstance!!, item.id)
+                        return@map key
+                    }
+                    return@map null
+                }.filterNotNull() }.flatten())
+                
+                playerOpeningEC[player.uniqueId] = true
+                val view = player.openWorkbench(null, true)
+                view?.title = "E_C"
+            }
+        }
+    }
     
+    @EventHandler
+    fun onInventoryClose(event: InventoryCloseEvent) {
+        val player = event.player
+        if (!(playerOpeningEC[player.uniqueId] ?: false)) return
+        val playerVanillaDiscoverdRecipe = playerVanillaDiscoverdRecipes[player.uniqueId] ?: return
+        player.undiscoverRecipes(player.discoveredRecipes)
+        player.discoverRecipes(playerVanillaDiscoverdRecipe)
+        playerOpeningEC[player.uniqueId] = false
+    }
+
     // io.github.thebusybiscuit.slimefun4.implementation.listeners.crafting.CraftingTableListener#onPrepareCraft priority = NORMAL
     @EventHandler(priority = EventPriority.HIGH)
     fun onPrepareItemCraft(event: PrepareItemCraftEvent) {
         val inventory = event.inventory
-        
+
         val matrix = inventory.matrix
         if (matrix.contentEquals(emptyMatrix)) return
 
         val players = inventory.viewers.filterNotNull()
         if (players.isEmpty()) return
         val player = players.component1()
-        PlayerProfile.fromUUID(player.uniqueId) { profile ->
+        
+        
+        if (playerOpeningEC[player.uniqueId] ?: false) {
+            // Opening EC
+            
+            // In EC can't craft vanilla items
+            // NEED TEST
+            inventory.result = null
+            
+            player.toSlimefun { profile ->
             val flatMatrix = matrix.flat
             Slimefun.getRegistry().enabledSlimefunItems.filterNotNull().forEach { item ->
                 if (item.recipeType != RecipeType.ENHANCED_CRAFTING_TABLE) return@forEach
@@ -54,8 +114,9 @@ class SlimevanillaListener(instance: Slimevanilla) : Listener {
                 }
             }
         }
+        }
     }
-    
+
     // io.github.thebusybiscuit.slimefun4.implementation.listeners.crafting.CraftingTableListener#onCraft priority = NORMAL
     @EventHandler(priority = EventPriority.HIGH)
     fun onCraft(event: CraftItemEvent) {
@@ -67,18 +128,5 @@ class SlimevanillaListener(instance: Slimevanilla) : Listener {
             event.result = Result.ALLOW
             event.isCancelled = false
         }
-    }
-
-    @EventHandler
-    fun onResearchUnlock(event: ResearchUnlockEvent) {
-        if (slimevanillaInstance == null) return
-        
-        event.player.discoverRecipes(event.research.affectedItems.map { item ->
-            if (item.recipeType == RecipeType.ENHANCED_CRAFTING_TABLE) {
-               val key = NamespacedKey(slimevanillaInstance!!, item.id)
-               return@map key
-            }
-            return@map null
-        }.filterNotNull())
     }
 }
