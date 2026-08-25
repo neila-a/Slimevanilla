22f22084c1b948bf430ca0d087f0251de25431ab
Neil Alen <neilaspace@outlook.com>
2026-08-25 15:12:39 +0800

    💩 支持类工作台多方块机器

--- src/main/kotlin/top/neila/slimevanilla/Slimevanilla.kt
+++ src/main/kotlin/top/neila/slimevanilla/Slimevanilla.kt
@@ -20,43 +20,41 @@ class Slimevanilla : SlimevanillaBase() {
     override fun getBugTrackerURL() = originalBugTrackerURL
 
     private fun addSlimefunRecipes() = Slimefun.getRegistry().enabledSlimefunItems.filterNotNull().forEach { item ->
-        if (item.recipeType == RecipeType.ENHANCED_CRAFTING_TABLE) {
-            val key = NamespacedKey(this, item.id)
-            val recipe = ShapedRecipe(key, item.recipeOutput)
+        val key = NamespacedKey(this, item.id)
+        val recipe = ShapedRecipe(key, item.recipeOutput)
 
-            val ingredients = mutableSetOf<ItemStack>()
-            val rows = arrayOf("", "", "")
-            var currentRow = 0
-            for (ingredient in item.recipe) {
-                if (rows[currentRow].length == rowWidth) {
-                    currentRow++
-                }
-                if (ingredient == null) {
-                    rows[currentRow] += " "
+        val ingredients = mutableSetOf<ItemStack>()
+        val rows = arrayOf("", "", "")
+        var currentRow = 0
+        for (ingredient in item.recipe) {
+            if (rows[currentRow].length == rowWidth) {
+                currentRow++
+            }
+            if (ingredient == null) {
+                rows[currentRow] += " "
+            } else {
+                if (ingredients.contains(ingredient)) {
+                    rows[currentRow] += startChar + ingredients.indexOf(ingredient)
                 } else {
-                    if (ingredients.contains(ingredient)) {
-                        rows[currentRow] += startChar + ingredients.indexOf(ingredient)
-                    } else {
-                        ingredients.add(ingredient)
-                        rows[currentRow] += startChar + ingredients.indexOf(ingredient)
-                    }
+                    ingredients.add(ingredient)
+                    rows[currentRow] += startChar + ingredients.indexOf(ingredient)
                 }
             }
-            recipe.shape(rows[0], rows[1], rows[2])
-            ingredients.forEachIndexed { index, ingredient ->
-                recipe.setIngredient(startChar + index, ingredient)
-            }
-
-            recipe.group = item.itemGroup.unlocalizedName
-            when (item.itemGroup.key.key) {
-                "weapons", "tools", "armor", "magical_armor"
-                    -> recipe.category = CraftingBookCategory.EQUIPMENT
-
-                "electricity", "androids", "cargo", "gps"
-                    -> recipe.category = CraftingBookCategory.REDSTONE
-            }
-            server.addRecipe(recipe)
         }
+        if (ingredients.isEmpty()) return@forEach
+        recipe.shape(rows[0], rows[1], rows[2])
+        ingredients.forEachIndexed { index, ingredient ->
+            recipe.setIngredient(startChar + index, ingredient)
+        }
+
+        when (item.itemGroup.key.key) {
+            "weapons", "tools", "armor", "magical_armor"
+                -> recipe.category = CraftingBookCategory.EQUIPMENT
+
+            "electricity", "androids", "cargo", "gps"
+                -> recipe.category = CraftingBookCategory.REDSTONE
+        }
+        server.addRecipe(recipe)
     }
 
     private fun translate() {
@@ -70,7 +68,7 @@ class Slimevanilla : SlimevanillaBase() {
     override fun onEnable() {
         translate()
         addSlimefunRecipes()
-        
+
         SlimevanillaListener(this)
     }
 }
--- src/main/kotlin/top/neila/slimevanilla/SlimevanillaListener.kt
+++ src/main/kotlin/top/neila/slimevanilla/SlimevanillaListener.kt
@@ -12,12 +12,25 @@ import org.bukkit.inventory.ItemStack
 import org.bukkit.inventory.MenuType.CRAFTING
 import org.bukkit.event.inventory.CraftItemEvent
 import io.github.thebusybiscuit.slimefun4.api.player.PlayerProfile
+import io.github.thebusybiscuit.slimefun4.implementation.items.altar.AncientAltar
+import io.github.thebusybiscuit.slimefun4.implementation.items.multiblocks.ArmorForge
+import io.github.thebusybiscuit.slimefun4.implementation.items.multiblocks.Compressor
 import io.github.thebusybiscuit.slimefun4.implementation.items.multiblocks.EnhancedCraftingTable
+import io.github.thebusybiscuit.slimefun4.implementation.items.multiblocks.GrindStone
+import io.github.thebusybiscuit.slimefun4.implementation.items.multiblocks.Juicer
+import io.github.thebusybiscuit.slimefun4.implementation.items.multiblocks.MagicWorkbench
+import io.github.thebusybiscuit.slimefun4.implementation.items.multiblocks.MakeshiftSmeltery
+import io.github.thebusybiscuit.slimefun4.implementation.items.multiblocks.OreCrusher
+import io.github.thebusybiscuit.slimefun4.implementation.items.multiblocks.OreWasher
+import io.github.thebusybiscuit.slimefun4.implementation.items.multiblocks.PressureChamber
+import io.github.thebusybiscuit.slimefun4.implementation.items.multiblocks.Smeltery
 import net.kyori.adventure.translation.GlobalTranslator
 import org.bukkit.NamespacedKey
 import org.bukkit.entity.HumanEntity
 import org.bukkit.entity.Player
 import org.bukkit.event.inventory.InventoryCloseEvent
+import org.bukkit.inventory.ShapedRecipe
+import org.bukkit.inventory.ShapelessRecipe
 import java.util.Locale
 import java.util.UUID
 import java.util.function.Consumer
@@ -42,24 +55,50 @@ class SlimevanillaListener(instance: Slimevanilla) : Listener {
     }
 
     private val playerVanillaDiscoverdRecipes = mutableMapOf<UUID, Collection<NamespacedKey>>()
-    private val playerOpeningEC = mutableMapOf<UUID, Boolean>()
+    private val playerOpening = mutableMapOf<UUID, RecipeType?>()
 
     @EventHandler(priority = EventPriority.LOW)
     fun onMultiBlockInteract(event: MultiBlockInteractEvent) {
         val multiBlock = event.multiBlock
         val player = event.player
+        val item = multiBlock.slimefunItem
+
+        var type: RecipeType? = null
+        if (item is EnhancedCraftingTable) {
+            type = RecipeType.ENHANCED_CRAFTING_TABLE
+        } else if (item is AncientAltar) {
+            type = RecipeType.ANCIENT_ALTAR
+        } else if (item is ArmorForge) {
+            type = RecipeType.ARMOR_FORGE
+        } else if (item is Compressor) {
+            type = RecipeType.COMPRESSOR
+        } else if (item is GrindStone) {
+            type = RecipeType.GRIND_STONE
+        } else if (item is Juicer) {
+            type = RecipeType.JUICER
+        } else if (item is MagicWorkbench) {
+            type = RecipeType.MAGIC_WORKBENCH
+        } else if (item is OreCrusher) {
+            type = RecipeType.ORE_CRUSHER
+        } else if (item is OreWasher) {
+            type = RecipeType.ORE_WASHER
+        } else if (item is Smeltery || item is MakeshiftSmeltery) {
+            type = RecipeType.SMELTERY
+        } else if (item is PressureChamber) {
+            type = RecipeType.PRESSURE_CHAMBER
+        }
+
         player.toSlimefun { profile ->
-            val item = multiBlock.slimefunItem
             if (!profile.hasUnlocked(item.research)) return@toSlimefun
 
-            if (item is EnhancedCraftingTable) {
+            if (type != null) {
                 event.isCancelled = true
 
                 playerVanillaDiscoverdRecipes[player.uniqueId] = player.discoveredRecipes
                 player.undiscoverRecipes(player.discoveredRecipes)
                 player.discoverRecipes(profile.researches.map { research ->
                     research.affectedItems.map { item ->
-                        if (item.recipeType == RecipeType.ENHANCED_CRAFTING_TABLE) {
+                        if (item.recipeType == type) {
                             val key = NamespacedKey(slimevanillaInstance!!, item.id)
                             return@map key
                         }
@@ -67,10 +106,11 @@ class SlimevanillaListener(instance: Slimevanilla) : Listener {
                     }.filterNotNull()
                 }.flatten())
 
-                playerOpeningEC[player.uniqueId] = true
+                playerOpening[player.uniqueId] = type
                 val view = player.openWorkbench(event.clickedBlock.location, true)
-                
-                val format = GlobalTranslator.translator().translate("container.enhanced_crafting", Locale.SIMPLIFIED_CHINESE)
+
+                val format =
+                    GlobalTranslator.translator().translate("container.enhanced_crafting", Locale.SIMPLIFIED_CHINESE)
                 val title = format?.format(emptyArray<Any>())
                 if (title != null)
                     view?.title = title
@@ -81,11 +121,11 @@ class SlimevanillaListener(instance: Slimevanilla) : Listener {
     @EventHandler
     fun onInventoryClose(event: InventoryCloseEvent) {
         val player = event.player
-        if (!(playerOpeningEC[player.uniqueId] ?: false)) return
+        if (playerOpening[player.uniqueId] == null) return
         val playerVanillaDiscoverdRecipe = playerVanillaDiscoverdRecipes[player.uniqueId] ?: return
         player.undiscoverRecipes(player.discoveredRecipes)
         player.discoverRecipes(playerVanillaDiscoverdRecipe)
-        playerOpeningEC[player.uniqueId] = false
+        playerOpening[player.uniqueId] = null
     }
 
     // io.github.thebusybiscuit.slimefun4.implementation.listeners.crafting.CraftingTableListener#onPrepareCraft priority = NORMAL
@@ -101,17 +141,13 @@ class SlimevanillaListener(instance: Slimevanilla) : Listener {
         val player = players.component1()
 
 
-        if (playerOpeningEC[player.uniqueId] ?: false) {
-            // Opening EC
-
-            // In EC can't craft vanilla items
-            // NEED TEST
+        if (playerOpening[player.uniqueId] != null) {
             inventory.result = null
 
             player.toSlimefun { profile ->
                 val flatMatrix = matrix.flat
                 Slimefun.getRegistry().enabledSlimefunItems.filterNotNull().forEach { item ->
-                    if (item.recipeType != RecipeType.ENHANCED_CRAFTING_TABLE) return@forEach
+                    if (item.recipeType != playerOpening[player.uniqueId]) return@forEach
 
                     val research = item.research
                     if (!profile.hasUnlocked(research)) return@forEach
